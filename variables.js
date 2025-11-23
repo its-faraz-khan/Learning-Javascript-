9dc04339c6367e9989440c9cf463b12258954ec9
its-faraz-khan <farazkhan4861@gmail.com>
2025-11-23 16:43:28 +0500

    Variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+const myName="Faraz";
+let myContact= 923437807333;
+var myAge = 19 //block scope and functional scope
+myEmail ="farazkhan4861@gmail.com";
+let DOB ;
+
+// myName ="Ahmad"
+
+console.table([myName, myAge , myContact , myEmail, DOB])
